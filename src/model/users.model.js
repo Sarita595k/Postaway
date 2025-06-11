7ad336a6c55ad08cbb4bac7ae701af9e1a477ba5
sarita595k <sarita595k@gmail.com>
2025-06-11 19:37:08 +0530

    user model created

--- src/model/users.model.js
+++ src/model/users.model.js
@@ -1,16 +1,16 @@
-// const users = []
+const users = []
 
-// export const userList = () => {
-//     return users
-// }
+export const userList = () => {
+    return users
+}
 
-// export const addUserInList = (data) => {
-//     const id = users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 1
-//     const newUser = { id, ...data }
-//     users.push(newUser)
-//     return newUser
-// }
+export const addUserInList = (data) => {
+    const id = users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 1
+    const newUser = { id, ...data }
+    users.push(newUser)
+    return newUser
+}
 
-// export const checkUserExist = async ({ email }) => {
-//     return users.find(user => user.email === email) || null
-// }
+export const checkUserExist = async ({ email }) => {
+    return users.find(user => user.email === email) || null
+}
